fd81ded2ee24222f5ece6649ec9e72a842401940
Martyna <martyna.sowinska@hotmail.com>
2021-09-14 17:13:04 +0200

    Homework Solution

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import cors from "cors";
+
+import listEndpoints from "express-list-endpoints";
+
+import authorsRouter from "./authors/index.js";
+
+const server = express();
+
+const PORT = 3001;
+
+server.use(cors());
+
+server.use(express.json());
+
+server.use("/authors", authorsRouter);
+
+console.log(listEndpoints(server));
+
+server.listen(PORT, () => console.log("✅ Server is running on port : ", PORT));
+
+server.on("error", (error) =>
+  console.log(`❌ Server is not running due to : ${error}`)
+);
